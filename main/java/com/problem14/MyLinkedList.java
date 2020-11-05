7407ab3783221716275194ecc7663ca43e2229bb
Sippora04 <sippora.toppo95@gmail.com>
2020-11-06 01:24:38 +0530

    Added: Welcome Message

---
+++ main/java/com/problem14/MyLinkedList.java
@@ -0,0 +1,10 @@
+package com.problem14;
+
+public class MyLinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome To Hash Table");
+	}
+
+}
